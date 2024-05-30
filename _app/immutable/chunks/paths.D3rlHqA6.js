81629e1447210635fc458018524d7a5382f20dfe
Jacob Hart <>
2024-05-30 05:49:16 +0200

    footer update

--- _app/immutable/chunks/paths.D3rlHqA6.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1mf52b9)==null?void 0:s.base)??"/sveltekit-github-pages";var e;const a=((e=globalThis.__sveltekit_1mf52b9)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.1Dzc1bB1.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CIFwmpO8.js";export{t as start};
